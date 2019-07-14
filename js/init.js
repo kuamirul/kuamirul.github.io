4f43f98c499ac9776281a4d29001cd700157348d
Ku Amirul <kuamirulkuamir@gmail.com>
2019-07-14 23:18:46 +0800

    update content

--- js/init.js
+++ js/init.js
@@ -6,3 +6,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
